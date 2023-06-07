fb176d90bc773412e05080ea0eff617802fbf3f9
Robert Strehlow <robstrehlow@msn.com>
2023-06-07 08:44:27 +0200

    data parser for TCG dataset with example use

--- model/data_parser.py
+++ model/data_parser.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 
 
 def get_data(file_dir):
@@ -17,9 +18,28 @@ def get_data(file_dir):
             with open(os.path.join(dirpath, js)) as json_file:
                 json_list.append(json.load(json_file))
 
-    print(json_list[799])
-
     return json_list
 
 
-get_data('../data/JAAD_output_JSON')
+def get_data_TCG(file_dir: str) -> (np.ndarray, dict):
+    """
+    Read in the TCG dataset and its annotation
+    :param file_dir: directory where data and annotations from TCG are stored
+    :return: Data as np array and annotations as dict
+    """
+    tcg_data = np.load(file_dir + 'tcg_data.npy', allow_pickle=True)
+
+    with open(file_dir + 'tcg.json') as json_file:
+        tcg_json = json.load(json_file)
+
+    return tcg_data, tcg_json
+
+
+# get_data('../data/JAAD_output_JSON')
+
+# example usage
+tcg_d, tcg_j = get_data_TCG('../data/TCG/')
+print(len(tcg_d))
+print(tcg_j.keys())
+print(len(tcg_j['sequences']))
+print(tcg_d[0][0])
