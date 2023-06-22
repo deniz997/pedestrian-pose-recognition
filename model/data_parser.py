8818a68d1767aeb14d78d12f2e7a44bc685535c5
Robert Strehlow <robstrehlow@msn.com>
2023-06-22 16:26:21 +0200

    added the show_skeleton function and a wacky transformation from BODY_25 to HRI coords

--- model/data_parser.py
+++ model/data_parser.py
@@ -1,8 +1,12 @@
 import os
 import json
 import numpy as np
+import sklearn.preprocessing
 from sklearn.preprocessing import normalize
 from openpose import op_utils
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from matplotlib.animation import FuncAnimation
 
 
 def get_JAAD_data(file_dir):
@@ -35,6 +39,7 @@ def get_data_TCG(file_dir: str) -> (np.ndarray, dict):
         tcg_json = json.load(json_file)
 
     return tcg_data, tcg_json
+
 
 def get_data_HRI(file_dir: str) -> (np.ndarray, list):
     '''
@@ -90,7 +95,92 @@ def load_data_HRI(file_dir: str) -> (np.ndarray, np.ndarray):
 
     return hri_data, hri_labels
 
+
+def transform_to_HRI(keypoints):
+    hri_dict = {'Nose': 0, 'LEye': 1, 'REye': 2, 'LEar': 3, 'REar': 4, 'LShoulder': 5, 'RShoulder': 6, 'LElbow': 7,
+                'RElbow': 8, 'LWrist': 9, 'RWrist': 10, 'LHip': 11, 'RHip': 12, 'LKnee': 13, 'RKnee': 14,
+                'LAnkle': 15, 'RAnkle': 16}
+    op_dict = {'Nose': 0, 'Neck': 1, 'RShoulder': 2, 'RElbow': 3, 'RWrist': 4, 'LShoulder': 5, 'LElbow': 6,
+               'LWrist': 7, 'MidHip': 8, 'RHip': 9, 'RKnee': 10, 'RAnkle': 11, 'LHip': 12, 'LKnee': 13, 'LAnkle': 14,
+               'REye': 15, 'LEye': 16, 'REar': 17, 'LEar': 18, 'LBigToe': 19, 'LSmallToe': 20, 'LHeel': 21,
+               'RBigToe': 22, 'RSmallToe': 23, 'RHeel': 24, 'Background': 25}
+    hri_keypoints = np.zeros((17, 2), dtype='float64')
+    for key, index in hri_dict.items():
+        hri_keypoints[index] = keypoints[op_dict[key]][:2]
+    # print((hri_keypoints - np.mean(hri_keypoints, axis=0)) / [1080, 1920])
+    hri_keypoints = (hri_keypoints - np.mean(hri_keypoints, axis=0)) / [1080, 1920] * 2
+    return hri_keypoints
+
+
+def show_skeleton(skeleton, reverse=True, as_video=False, save_with_name="", frame=0):
+    ###
+    # Skeleton: (frame_count, 17, num of dimentions, e.g. 2)
+    # reverse: The plot is upside down, set it to False when the original plot is needed
+    # as_video: Generates and displays a video from the skeleton sequences
+    # save_with_name: Saves a video from the skeleton sequences with the given file name,
+    #                 ignored if as_video is set to False
+    # frame: the frame number to plot the skeleton from,
+    #         should not exceed the total frame count of the sequence
+    ###
+
+    frame_count = skeleton.shape[0]  # Total frame count
+    if frame_count <= frame:
+        print("Frame count exceeded!")
+        return
+
+    # Create a new figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Define the connections between keypoints (assuming a specific order)
+    connections = [(0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6), (5, 7), (6, 8),
+                   (7, 9), (8, 10), (5, 11), (6, 12), (5, 6), (11, 12), (11, 13),
+                   (12, 14), (13, 15), (14, 16)]
+
+    def update_frame(updated_frame):
+        ax.cla()
+        # Example skeleton data
+        skeleton_data = np.array(skeleton)[
+            updated_frame]  # List of skeleton data, each element is a matrix of shape (Frame count, 17,
+        # 2) representing the 2D locations
+
+        if reverse:
+            skeleton_data[:, 1] = skeleton_data[:, 1] * -1
+
+        # Plot keypoints
+        ax.scatter(skeleton_data[:, 0], skeleton_data[:, 1], marker='o')
+
+        # Plot connections
+        for connection in connections:
+            ax.plot([skeleton_data[connection[0], 0], skeleton_data[connection[1], 0]],
+                    [skeleton_data[connection[0], 1], skeleton_data[connection[1], 1]])
+
+        # Add keypoint labels
+        for j, point in enumerate(skeleton_data):
+            ax.text(point[0], point[1], str(j), fontsize=8)
+        # Set plot limits and labels
+        ax.set_xlim([-1, 1])  # Specify the width of your image
+        ax.set_ylim([-1, 1])  # Specify the height of your image
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+
+    if as_video:
+        rc('animation', html='jshtml')
+        animation = FuncAnimation(fig, update_frame, frames=frame_count, interval=200)
+        if save_with_name != "":
+            animation.save(save_with_name + ".mp4")
+        return animation
+    else:
+        update_frame(frame)
+        plt.show()
+
+
 # example usage
-#data, lab = load_data_HRI('data/HRI_gestures/')
-#print(data.shape)
-#print(lab.shape)
+# data = np.load('../data/hri_keypoints_robert.npy', allow_pickle=True)
+# show_skeleton(data[0][0][None])
+
+hri_labels = ['Come Here', 'Follow Me', 'Follow Me', 'Follow Me', 'Follow Me',
+              'Get Attention', 'Get Attention', 'Get Attention', 'Go Left', 'Go Left',
+              'Go Right', 'Go Right', 'Standing Still', 'Standing Still', 'Standing Still',
+              'Stop', 'Stop', 'Stop', 'Stop', 'Stop']
+np.save('../data/hri_labels_robert.npy', np.array(hri_labels, dtype='str'))
