a60d2cf8381f72e4f0b403ee11e6eed212350cf2
bartelmu <max.002@web.de>
2023-07-05 14:09:39 +0200

    add lstm

--- model/action_recognition/simple_classifiers.py
+++ model/action_recognition/simple_classifiers.py
@@ -2,6 +2,7 @@ import numpy as np
 import sklearn.metrics as metrics
 from scipy import sparse
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from skmultilearn.problem_transform import BinaryRelevance, ClassifierChain
@@ -35,20 +36,42 @@ if __name__ == '__main__':
     model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
-    model.add(Dense(5))  # output layer，units is the unit number for output
+    model.add(Dense(1))  # output layer，units is the unit number for output
 
     # compile model
     optimizer = keras.optimizers.Adam(learning_rate=0.00001)
     model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
 
+    for col in Y_train.columns:
+        y_train = Y_train[col].to_numpy()
+        y_test = Y_test[col].to_numpy()
+        model.fit(X_train, y_train, epochs=11, batch_size=32)
+
+        # prediction
+        predictions = model.predict(X_test)
+
+        print(predictions)
+        print("Evaluate on test data for col: " + col)
+        results = model.evaluate(X_test, y_test, batch_size=128)
+        print(dict(zip(model.metrics_names, results)))
+
     # train model
-    model.fit(X_train, Y_train, epochs=11, batch_size=32)
-
-    # prediction
-    predictions = model.predict(X_test)
-
-    print("Evaluate on test data")
-    results = model.evaluate(X_test, Y_test, batch_size=128)
-    print(dict(zip(model.metrics_names, results)))
-
-    round_pred = np.round(predictions).astype(int)
+    # model.fit(X_train, Y_train, epochs=11, batch_size=32)
+    #
+    # # prediction
+    # predictions = model.predict(X_test)
+    #
+    # print("Evaluate on test data")
+    # results = model.evaluate(X_test, Y_test, batch_size=128)
+    # print(dict(zip(model.metrics_names, results)))
+    # i = 0
+    # round_pred = np.round(predictions).astype(int)
+    # for col in Y_test.columns:
+    #     print(col + " accury:")
+    #     print(metrics.accuracy_score(Y_test[col], round_pred[:,1]))
+    #     i += 1
+        # Y_test_new = Y_test.to_numpy()
+        # y_prediction = np.argmax(round_pred[:,1])
+        # y_test = np.argmax(Y_test_new[:,1], axis=1)
+        # result = confusion_matrix(y_test, y_prediction, normalize='pred')
+        # print(result)
